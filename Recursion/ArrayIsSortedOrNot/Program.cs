e02156f087bc9e356b894c903f64c35d886729b9
ParminderSingh0191 <57814660+ParminderSingh0191@users.noreply.github.com>
2021-04-22 22:43:56 -0400

    Check array is sorted or not using recursion

---
+++ Recursion/ArrayIsSortedOrNot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayIsSortedOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = IsArraySortedOrNot(new int[] { 1, 2, 3, 4, 5, 6, 8, 7 }, 8);
+
+            if(result == 1)
+            {
+                Console.WriteLine("Array of items is sorted.");
+            }
+            else
+            {
+                Console.WriteLine("Array of items is not sorted.");
+            }
+        }
+
+        private static int IsArraySortedOrNot(int[] items, int n)
+        {
+            if(n==1)
+            {
+                return 1;
+            }
+
+            return (items[n - 1] < items[n - 2]) ? 0 : IsArraySortedOrNot(items, n - 1);
+        }
+    }
+}
