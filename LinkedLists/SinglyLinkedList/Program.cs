af46aa003b21431100162a0774886936372fdf8f
ParminderSingh0191 <57814660+ParminderSingh0191@users.noreply.github.com>
2021-04-22 23:10:49 -0400

    Added singly linked list

---
+++ LinkedLists/SinglyLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SinglyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+    public class Node
+    {
+        internal int data;
+        internal Node Next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            Next = null;
+        }
+    }
+}
