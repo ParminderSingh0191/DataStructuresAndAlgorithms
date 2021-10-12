554263f01869b9b6fe14bf521dd8d0d7e832b2e5
Parminder Singh <parmindersingh0191@gmail.com>
2021-10-12 13:55:59 -0400

    Remove ignored files

---
+++ DoublyLinkedList.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoublyLinkedList.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
